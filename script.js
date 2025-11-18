bbe195616b4ae01b0f1521dbd905245aa8d977fb
NullRoot <luis.escare@virginiogomez.cl>
2025-11-18 12:34:43 -0300

    Añadido de seccion Sobre Nosotros, correccion y meoramiento del comportamiento de los elementos cuando interactuan

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const btn = document.getElementById("hamburgerBtn");
 const menu = document.getElementById("menu");
 const overlay = document.getElementById("overlay");
+const goDownBtn =document. getElementById("go_down_btn")
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active'); // Alterna la clase 'active' en el side-menu
     overlay.style.display = menu.classList.contains('active') ? 'block' : 'none';
+    goDownBtn.classList.toggle('visible');
+
 });
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
     overlay.style.display = 'none';
+    goDownBtn.classList.toggle('visible');
+
+
 });
 function scrollToInfo() {
     const infoSection = document.getElementById("info");
     infoSection.scrollIntoView({ behavior: "smooth" });
+
 }
 
